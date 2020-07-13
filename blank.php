f47d2a6c33f3349e06674069df36288d3a097b2a
Matías Sanchez Moisés <saitam06@gmail.com>
2020-07-12 21:32:36 -0500

    update on blank template

--- blank.php
+++ blank.php
@@ -1,21 +1,29 @@
 <?php /* Template Name: Blank */ ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width" />
     <?php wp_head(); ?>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150327458-1"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-150327458-1');
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-150327458-1');
     </script>
 </head>
-  <body>
-    <h1 class="entry-title"><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-  </body>
+<body>
+    <main id="content" class="category">
+        <div class="container has-content" id="container">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <article>
+                <?php the_content(); ?>
+            </article>
+        </div>
+    </main>
+</body>
 </html>
